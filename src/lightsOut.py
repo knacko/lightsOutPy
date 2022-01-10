eeec874f28cadd5c529f5b07489a3a2d8254b712
knacko <geekbike@hotmail.com>
2022-01-10 12:15:03 -0700

    Added octogon-square

--- src/lightsOut.py
+++ src/lightsOut.py
@@ -243,6 +243,43 @@ class PolygonManager:
                 AdjPolygon(shape="Hexagon", angle=180, distance=2*centerToTop, rotation=0),
                 AdjPolygon(shape="Hexagon", angle=240, distance=2*centerToTop, rotation=0),
                 AdjPolygon(shape="Hexagon", angle=300, distance=2*centerToTop, rotation=0)]
+        elif shape == "SquareOctogon":
+            squareUnitLength = 0.75  # This must match the value in SquareOctogon
+            axisToAngle = squareUnitLength * math.sin(math.radians(45)) / math.sin(math.radians(90))
+            points = [(0, axisToAngle),
+                      (axisToAngle, 0),
+                      (0, -axisToAngle),
+                      (-axisToAngle, 0)]
+            squareToOct = squareUnitLength + axisToAngle
+            adjPolygons = [
+                AdjPolygon(shape="OctogonSquare", angle=45,  distance=squareToOct, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=135, distance=squareToOct, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=225, distance=squareToOct, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=315, distance=squareToOct, rotation=0)]
+        elif shape == "OctogonSquare":
+            squareUnitLength = 0.75  # This must match the value in OctogonSquare
+            axisToAngle = squareUnitLength * math.sin(math.radians(45)) / math.sin(math.radians(90))
+            squareUnitLength /= 2
+            axisToAngle += squareUnitLength
+            points = [(-squareUnitLength, axisToAngle),
+                      (squareUnitLength, axisToAngle),
+                      (axisToAngle, squareUnitLength),
+                      (axisToAngle, -squareUnitLength),
+                      (squareUnitLength, -axisToAngle),
+                      (-squareUnitLength, -axisToAngle),
+                      (-axisToAngle, -squareUnitLength),
+                      (-axisToAngle, squareUnitLength)]
+            octToOct = 2 * axisToAngle
+            octToSquare = axisToAngle + squareUnitLength
+            adjPolygons = [
+                AdjPolygon(shape="OctogonSquare", angle=0, distance=octToOct, rotation=0),
+                AdjPolygon(shape="SquareOctogon", angle=45, distance=octToSquare, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=90, distance=octToOct, rotation=0),
+                AdjPolygon(shape="SquareOctogon", angle=135, distance=octToSquare, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=180, distance=octToOct, rotation=0),
+                AdjPolygon(shape="SquareOctogon", angle=225, distance=octToSquare, rotation=0),
+                AdjPolygon(shape="OctogonSquare", angle=270, distance=octToOct, rotation=0),
+                AdjPolygon(shape="SquareOctogon", angle=315, distance=octToSquare, rotation=0)]
 
         def scalePos(point):
             (x, y) = point
@@ -262,7 +299,7 @@ class LightsOut:
 
     def __init__(self, fname=None):
         position = tuple(x / 2 for x in pygame.display.get_surface().get_size())
-        self.polyMan.createPolygon("Hexagon", position = position)
+        self.polyMan.createPolygon("OctogonSquare", position = position)
 
     def drawGame(self):
         for polygon in self.polygons:
